ba3a8d3ff4fc9ab08f09e9803d93897343751d1f
Dru Hill <dru.hill@sage.com>
2020-04-12 00:04:27 +0100

    eject and initial score

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+function createData(team, score, movement) {
+  return { team, score, movement };
+}
+
+const rows = [
+  createData('Dru & Sarah', 60, 0),
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Typography variant="h3" gutterBottom>
+          Brew Quiz
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Team</TableCell>
+                <TableCell align="right">Total score</TableCell>
+                <TableCell align="right">Movement</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.team}>
+                  <TableCell component="th" scope="row">
+                    {row.team}
+                  </TableCell>
+                  <TableCell align="right">{row.score}</TableCell>
+                  <TableCell align="right">{row.movement}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 }
